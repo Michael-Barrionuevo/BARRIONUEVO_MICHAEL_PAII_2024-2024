1bd7b823bb4c1a97b9837c7f10711e45652b570e
veykerbarrionuevo <veykerbarrionuevo@192.168.100.20>
2024-04-16 22:44:22 -0500

    repaso

--- Proyecto_001/src/comp001/main.java
+++ Proyecto_001/src/comp001/main.java
@@ -4,7 +4,8 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("No hay promoción de regalo para este producto.");
+
+		System.out.println("Hola mundo");
 	}
 
 }
