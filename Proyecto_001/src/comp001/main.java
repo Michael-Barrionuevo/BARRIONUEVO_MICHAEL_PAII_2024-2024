775a9dac2fd94a1f9c1980705c2f47e60f6bc946
veykerbarrionuevo <veykerbarrionuevo@192.168.100.20>
2024-04-16 22:34:25 -0500

    repasin

---
+++ Proyecto_001/src/comp001/main.java
@@ -0,0 +1,10 @@
+package comp001;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("No hay promoción de regalo para este producto.");
+	}
+
+}
