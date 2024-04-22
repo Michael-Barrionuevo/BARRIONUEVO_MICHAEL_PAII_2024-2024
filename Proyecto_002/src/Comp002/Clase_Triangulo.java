c981ba2357ae08e7155218702f4343e63444a8b3
veykerbarrionuevo <veykerbarrionuevo@192.168.100.20>
2024-04-22 10:31:25 -0500

    Proyecto2

---
+++ Proyecto_002/src/Comp002/Clase_Triangulo.java
@@ -0,0 +1,17 @@
+package Comp002;
+
+import Interfaz.InterfaceDibujarFiguras;
+
+public class Clase_Triangulo implements InterfaceDibujarFiguras {
+
+	@Override
+	public void Dibujar() {
+		
+		System.out.println("Triangulo ");
+		
+	}
+
+	 
+	
+	
+}
